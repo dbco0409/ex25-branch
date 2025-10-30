c974c24d2f8931c990be54feda706cb58a60e4f4
dbco0409 <75134167+dbco0409@users.noreply.github.com>
2025-10-30 16:29:49 +0900

    add db

--- src/main/java/com/hkd/mapper/StudentMapper.java
+++ src/main/java/com/hkd/mapper/StudentMapper.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.skc.mybatis.mapper;
 
 import java.util.List;
@@ -30,4 +31,34 @@ public interface StudentMapper {
 	void delete(Long id);
 //	@Delete("DELETE FROM student WHERE id = #{id}")
 //	void delete(Long id);
+=======
+package com.hkd.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.hkd.domain.Student;
+
+@Mapper
+public interface StudentMapper {
+	@Select("select * from student order by id desc")
+	List<Student> getAllStudent();
+	
+	@Select("select * from student where id = #{id}")
+	Student getStudent(int id);
+	
+	@Insert("insert into student (name, email, age, createdAt) values (#{name}, #{email}, #{age}, Now()) ")
+	void createStudent(Student student);
+	
+	@Update("update student set name = #{name}, email = #{email}, age = #{age}, updatedAt = now() where id=#{id}")
+	void updateStudent(Student student);
+	
+	@Delete("delete from student where id = #{id}")
+	void deleteStudent(int id);
+>>>>>>> develop
 }
